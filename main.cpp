d2fd90883ad759162719aa1ed8df5d17d67f76d6
Andrusbek1337 <shalashovandrew@gmail.com>
2025-12-17 23:39:51 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <limits>
+#include "hashset.h"
+#include "dictionary.h"
+#include "task1.h"
+#include "task2.h"
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    //задание 1
+    solveTask1();
+
+    //задание 2
+    solveTask2();
+
+    return 0;
+}
